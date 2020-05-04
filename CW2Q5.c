7dee56c9ec9c78e726e158e74f768fbfc7fd7c07
Freddie Jonas <fj6787@gmail.com>
2020-05-04 20:52:41 +0100

    Added procedures to remove set of words from a list of text
    Error caused by infinite loop being caused by struct pointer.

--- CW2Q5.c
+++ CW2Q5.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+typedef int bool;
+#define true 1
+#define false 0
+
+struct RedactedListItem {
+    int charIndex;
+    int lastItem;
+    struct RedactedListItem *nextItem;
+};
+
+// checks for terminating value and returns the length
 int stringLength(char* ptr) {
     int length = 0;
     while (ptr[length] != '\0') {
@@ -8,37 +19,76 @@ int stringLength(char* ptr) {
     return length;
 }
 
-char * removeWords(char *text, char *redactWords) {
-    char currentWord[stringLength(text)];
-    int currentWordIndex = 0;
-    int currentTextIndex = 0;
-    int len = stringLength(text);
-    for (int i = 0; i < len; i ++) {
-        //printf("Text %c\n", text[i]);
-        if (text[i] != ' ') {
-            currentWord[currentWordIndex] = text[i];
-            currentWordIndex += 1;
-        }
-        else {
-            // compare redacted words against words in text
-
-
-            currentTextIndex += currentWordIndex + 1;
-            currentWordIndex = 0;
-
-
-        }
-        printf("%s\n", currentWord);
-
+char *clearArray(char* array) {
+    for (int i = 0; i < stringLength(array); i++) {
+        array[i] = '\0';
     }
-
-
-    return 0;
+    return array;
 }
 
-int main() {
-    char *text = "The quick brown fox jumps over the lazy dog";
-    char *redactWords = "the jumps lazy";
-    char *textPtr = removeWords(text, redactWords);
+void setupRedactedList(struct RedactedListItem* firstItem, char* redactedWords) {
+    int len = stringLength(redactedWords);
+    firstItem->charIndex = 0;
+    struct RedactedListItem *thisItem = firstItem;
+    for (int i = 1; i < len; i++) {
+        if (redactedWords[i] == ' ') {
+            struct RedactedListItem newItem;
+            newItem.charIndex = i;
+            newItem.lastItem = 0;
+            thisItem->nextItem = &newItem;
+            thisItem = &newItem;
+        }
+    }
+    thisItem->lastItem = 1;
+    printf("Set for index %d\n", thisItem->charIndex);
+}
+
+int lengthToNextWord(char *text, int index) {
+    int num = 0;
+    while (text[index + num] != ' ') {
+        num++;
+    }
+    return num + 1;
+}
+
+void removeWords(char* text, char *redactWords, struct RedactedListItem* firstItem) {
+    int len = stringLength(text);
+    for (int i = 0; i < len; i = i + lengthToNextWord(text, i)) {
+        printf("%d\n", i);
+        int wordStart = i;
+        struct RedactedListItem *currentItem;
+        currentItem = firstItem;
+        while (currentItem->lastItem == 0) {
+            printf("%d\n", currentItem->lastItem);
+        //     bool matched = false;
+        //     int currentIndex = currentItem->charIndex;
+        //     while (text[i] == redactWords[currentIndex]) {
+        //         if (text[i] == ' ' && redactWords[currentIndex] == ' ') {
+        //             for (int j = wordStart; j < i; j++) {
+        //                 text[j] = '*';
+        //             }
+        //             matched = true;
+        //             continue;
+        //         }
+        //         i++;
+        //         currentIndex++;
+        //     }
+        //     if (matched) {
+        //         continue;
+        //     } else {
+        //         i = wordStart;
+        //     }
+            currentItem = currentItem->nextItem;
+        }
+    }
+}
+
+int main(void) {
+    char text[] = "The quick brown fox jumps over the lazy dog";
+    char redactWords[] = "the jumps lazy";
+    struct RedactedListItem firstItem;
+    setupRedactedList(&firstItem, redactWords);
+    removeWords(text, redactWords, &firstItem);
+    printf("%s", text);
     return 0;
 }
