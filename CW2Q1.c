c26bfb3820383a6cc27f1f8b66c835cf491ceb55
Freddie Jonas <fj6787@gmail.com>
2020-05-10 19:56:26 +0100

    Commenting

--- CW2Q1.c
+++ CW2Q1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// calculates the length of a char pointer
 int stringLength(char* ptr) {
     int length = 0;
     while (ptr[length] != '\0') {
@@ -8,17 +9,17 @@ int stringLength(char* ptr) {
     return length;
 }
 
-//https://www.udemy.com/blog/c-string-to-int/
+// converts string to an interger, based upon https://www.udemy.com/blog/c-string-to-int/
 int stringToInt(char* ptr) {
     int len = stringLength(ptr);
     int result = 0;
     for (int i = 0; i < len - 1; i++) {
         result = result * 10 + (ptr[i] - '0');
-        //printf("Result %d\n", result);
     }
     return result;
 }
 
+// finds the power of two values
 int intPow(int base, int exponent) {
     int result = 1;
     for (int i = 0; i < exponent; i++) {
@@ -27,37 +28,49 @@ int intPow(int base, int exponent) {
     return result;
 }
 
+// converts integer to string
 void intToString(char* result, int num, int len) {
+    // converts each character to the string version of itself, starting at the end and moving to the start
     for (int i = 0; i < len; i++) {
         int digit = len - i - 1;
+        // rem stores the last digit of the number
         int rem = num % 10;
+        // this is then added to the string at the end position
         result[digit] = '0' + rem;
+        // the number is then updated to remove this digit
         num = (num - rem) / 10;
     }
 }
 
+// creates a random number
 // assume seed is always 4 digit value
 int createRand(int seed) {
     int nextRand = seed * seed;
     // 8 digits long as 9999^2 is 8 digits total
     int len = 8;
     char nextString[len];
+    // convert to string so can take each number character
     intToString(nextString, nextRand, len);
 
-    // pad to 8 long using 0s
+    // takes the digits 3-7 as the next random number seed
     char ret[4] = {
         nextString[2],
         nextString[3],
         nextString[4],
         nextString[5]
     };
+    // convert back to integer
     int returnNum = stringToInt(ret);
+    // return the random number
     return returnNum;
 }
 
+// generates 100 random numbers
 int *generate100Rand() {
     static int numbers[99];
+    // starting seed of 1111
     int seed = 1111;
+    // loop 100 times to produce 100 numbers
     for (int i = 0; i < 99; i++) {
         numbers[i] = createRand(seed);
         seed = numbers[i];
@@ -65,6 +78,7 @@ int *generate100Rand() {
     return numbers;
 }
 
+// swap two integer values around
 int *swap(int *toSwapPointer, int indexA, int indexB) {
     int temp = toSwapPointer[indexB];
     toSwapPointer[indexB] = toSwapPointer[indexA];
@@ -78,44 +92,51 @@ int *cocktailSort(int *ptr) {
     int swapped;
     do {
         swapped = 0;
+        // loop through the random numbers start -> end
         for (int i = 0; i < 99; i++) {
+            // checks if a swap can happen, if it can it swaps and sets swapped to 1
             if (ptr[i] > ptr[i+1]) {
                 ptr = swap(ptr, i, i + 1);
                 swapped = 1;
             }
         }
+        // if no swaps have been made then list is sorted, exit do loop
         if (swapped == 0) {
             break;
         }
+        // reset swapped to 0
         swapped = 0;
+        // loop through random numbers end -> start
         for (int i = 98; i >= 0; i--) {
+            // checks if a swap can happen, if it can it swaps and sets swapped to 1
             if (ptr[i] > ptr[i+1]) {
                 ptr = swap(ptr, i, i + 1);
                 swapped = 1;
             }
         }
+        // loops while swapped is set to 1
     } while (swapped == 1);
+    // return the pointer
     return ptr;
 }
 
 
 int main(void) {
-    //char test = createRand(2343);
-    //printf("\n%d\n", test);
-
+    // generate 100 random numbers
     int *ptr;
     ptr = generate100Rand();
+    // prints unsorted list
     printf("Unsorted List: ");
     for (int i = 0; i < 100; i++ ) {
       printf( "Number: %d : %d\n", i+1, *(ptr + i));
     }
+    // sorts
     ptr = cocktailSort(ptr);
 
+    // output sorted list
     for (int i = 0; i < 100; i++ ) {
       printf( "Number: %d : %d\n", i+1, *(ptr + i));
     }
 
-    //char ret[4] = "4896";
-    //printf("%d", stringToInt(ret));
     return 0;
 }
