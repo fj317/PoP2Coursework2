5730aa16e7a09dd4ad721a45ded25dd86b3b3c54
Freddie Jonas <fj6787@gmail.com>
2020-05-10 19:45:29 +0100

    Commenting added

--- CW2Q7.c
+++ CW2Q7.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h> 
 
+// finds the length of a string
 int stringLength(char* pointer) {
     int length = 0;
     while (pointer[length] != '\0') {
@@ -9,6 +9,7 @@ int stringLength(char* pointer) {
     return length;
 }
 
+// swaps two elements in a char array with each other
 char *swap(char *toSwapPointer, char indexA, char indexB) {
     char temp = toSwapPointer[indexB];
     toSwapPointer[indexB] = toSwapPointer[indexA];
@@ -16,15 +17,20 @@ char *swap(char *toSwapPointer, char indexA, char indexB) {
     return toSwapPointer;
 }
 
+// bubble sort to sort the keyword columns into their alphabetical order
 void bubbleSortText(char *key, char (*columns)[stringLength(key)], int rowNum) {
     char tempCharStore;
     // boolean - 0 is false, 1 is true
     int swapped;
+    // loop until no swaps are made, therefore keyword and columns are sorted
     do {
         swapped = 0;
+        // loop through each character of the key
         for (int i = 0; i < stringLength(key) - 1; i++) {
+            // if the current key character is larger than the next, swap them
             if (key[i] > key[i+1]) {
                 key = swap(key, i, i + 1);
+                // swap all items in the columns around as well
                     for (int j = 0; j < rowNum; j++) {
                         tempCharStore = columns[j][i];
                         columns[j][i] = columns[j][i + 1];
@@ -43,10 +49,14 @@ unsigned int roundUp(int dividend, int divisor)
     return (dividend + (divisor - 2)) / divisor;
 }
 
+// puts the text into column format
 void columnCreate(char key[], char text[], char (*columns)[stringLength(key)]) {
+    // gets the row and column numbers
     int rowNum = roundUp(stringLength(text), stringLength(key));
     int columnNum = stringLength(key);
+    // stores current point in text
     int pointInText = 0;
+    // loops through each row and column assigning the current location in the text to that index
     for (int i = 0; i < rowNum; i++) {
         for (int j = 0; j < columnNum; j++) {
             columns[i][j] = text[pointInText];
@@ -55,12 +65,16 @@ void columnCreate(char key[], char text[], char (*columns)[stringLength(key)]) {
     }
 }
 
+// pads the message
 void padMessage(char *text, char *padText, int paddedLength) {
+    // stores the text length
     int textLength = stringLength(text);
+    // sets the pad text to the text already
     for (int i = 0; i < textLength; i++) {
         padText[i] = text[i];
     }
     int count = textLength;
+    // adds extra 'X' character to the end if there is space allocated for it
     while (count != paddedLength) {
         padText[count] = 'X';
         count++;
@@ -75,9 +89,10 @@ char toUpper(char character) {
     return character;
 }
 
+
 int main(void) {
     // file reading
-    int c;
+    char c;
     FILE *file;
     file = fopen("Files/Q7/text.txt", "r");
     // finds the length of the file
@@ -86,36 +101,51 @@ int main(void) {
     fseek(file, 0L, SEEK_SET);
     // resizes text to the size of the file
     char text[fileLength];
-    //char *text = (char*) malloc ((fileLength + 1) * sizeof(char));
+    // counts position in file and position in text
     int fileCounter = 0;
     int textCounter = 0;
     if (file) {
+        // while not at end of file
         while (fileCounter < fileLength) {
+            // turn character read from file to upper case
             c = toUpper(getc(file));
+            // if it is uppercase add it to the text, otherwise discard
             if (c >= 65 && c <= 90) {
                 text[textCounter++] = c;
             }
+            // add 1 to the file counter
             fileCounter++;
-            //printf("char %c\n", c);
         }
+        // close file reader
         fclose(file);
     }
 
-    //char text[] = "It will now be inquired how the machine can of itself, and without having recourse to the hand of man, assume the successive dispositions suited to the operations. The solution of this problem has been taken from Jacquard's apparatus, used for the manufacture of brocaded stuffs, in the following manner.";
+    // KEY FOR ENCRYPTION OF THE TEXT
     char key[] = "LOVELACE";
     
+    // stores what the length of the padded word should be
     int paddedLength = stringLength(key) * roundUp(stringLength(text), stringLength(key)) + 1;
-    char *paddedText = (char*) malloc (paddedLength * sizeof(char));
+    // allocates space for word
+    char paddedText[paddedLength];
+    // creates the padded text
     padMessage(text, paddedText, paddedLength);
+    // output to console
     printf("Pad message: %s\n", paddedText);
 
+    // works out the number of rows of table
     int rowNum = roundUp(stringLength(paddedText), stringLength(key));
+    // works out number of columns of table
     int columnNum = stringLength(key);
+    // stores the table data 
     char columns[rowNum][columnNum];
 
     printf("Key: %s.\n", key);
+    // creates the columns the text are stored in
     columnCreate(key, paddedText, columns);
+    // sort the columns by the key
     bubbleSortText(key, columns, rowNum);
+    // output the columns in the sorted order
+    // variable used to store the for loop max length
     int forLength = stringLength(paddedText) / stringLength(key);
     printf("Cipher text: ");
     for (int i = 0; i < stringLength(key); i++) {
