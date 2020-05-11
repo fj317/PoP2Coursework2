89baa367914666feeec8168ff4a6043cc2fd9a99
Freddie Jonas <fj6787@gmail.com>
2020-05-11 12:22:08 +0100

    File reading added

--- CW2Q3.c
+++ CW2Q3.c
@@ -41,6 +41,7 @@ bool search(unsigned char name[], struct dataItem* hashArray, unsigned long maxT
     return true;
 } 
 
+// setup dataItem so it stores the name to input as struct which contains a char array
 void setupDataItem(unsigned char name[], struct dataItem *firstChar) {
     int len = stringLength(name);
     int count = 0;
@@ -54,11 +55,12 @@ void setupDataItem(unsigned char name[], struct dataItem *firstChar) {
 struct dataItem *addName(unsigned char name[], struct dataItem *hashArray, unsigned long *tableSize) {
     unsigned long hashResult = hash(name);
     if (hashResult > *tableSize) {
-        hashArray = (struct dataItem*) realloc(hashArray, sizeof(struct dataItem) * hashResult);
+        hashArray = realloc(hashArray, sizeof(struct dataItem) * hashResult);
         *tableSize = hashResult;
     }
     struct dataItem *firstChar = (struct dataItem*) malloc(sizeof(struct dataItem));
     setupDataItem(name, firstChar);
+    printf("Size of dataitem %lu vs size of firstChar %lu\n", sizeof(struct dataItem), sizeof(firstChar));
     hashArray[hashResult - 1] = *firstChar;
     printf("Data item %s has been added.\n", name);
     return hashArray;
@@ -79,11 +81,71 @@ void removeName(unsigned char name[], struct dataItem* hashArray, unsigned long 
 
 
 int main(void) {
-    unsigned char text[] = "Freddie";
+    // hash table declaration
     unsigned long tableSize = 1;
-
     struct dataItem *hashArray = (struct dataItem*) malloc(tableSize * sizeof(struct dataItem));
+    
+    // READ NAMES.TXT
+    char c;
+    FILE *file = fopen("Files/Names/names.txt", "r");
+    // finds the length of the file
+    fseek(file, 0L, SEEK_END);
+    int fileLength = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+    int currentSize = sizeof(char);
+    // resizes text to the size of the file + 1 (plus 1 as need to add a newline at the end of the char)
+    unsigned char *name = malloc (currentSize);
+    //printf("length %d, Size 1 %lu, size 2 %lu, sizeOf(char) %lu\n", stringLength(redactWords), sizeof(redactWords) * sizeof(char), sizeof(char) * (fileLength + 1), sizeof(char));
+    // counts position in file and position in text
+    int fileCounter = 0;
+    int textCounter = 0;
+    if (file) {
+        // while not at end of file
+        while (fileCounter < 16) {
+            c = getc(file);
+            if (c == ',') {
+                printf("Current string %s\n", name);
+                // add the current name to the hash table
+                hashArray = addName(name, hashArray, &tableSize);
+                // deallocate memory as new word to add
+                free(name);
+                // resize back to one char
+                currentSize = sizeof(char);
+                // allocate space for the char
+                name = malloc (currentSize);
+                // reset textcounter to 0 as new word starts at 0
+                textCounter = 0;
+                // continue with the next character
+                continue;
+            } else if (c == '"') {
+                // if speechmarks do not add to the word, move to next character in file
+                fileCounter ++;
+                continue;
+            } else {
+                //printf("Current char %c\n", c);
+                // update currentSize to add the extra character
+                currentSize += sizeof(char);
+                // otherwise allocte space for the character
+                name = realloc(name, currentSize);
+                // add it to the name
+                name[textCounter++] = c;
+            }
+            fileCounter++;
+        }
+        // close file reader
+        fclose(file);
+    }
+    //printf("Text is %s\n", name);
+
+
+    unsigned char text[] = "Freddie";
+    unsigned char text2[] = "Abe";
+
+
+    
     hashArray = addName(text, hashArray, &tableSize);
+    hashArray = addName(text2, hashArray, &tableSize);
+
     removeName(text, hashArray, tableSize);
     unsigned long hashIndex = hash(text) - 1;
     //printf("Is there a value for Freddie? %d", search(text, hashArray, tableSize));
